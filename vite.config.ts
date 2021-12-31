1fa16cae66bad27b6d9d7ced784deffb72ffa103
ratchagarn <ratchagarn@gmail.com>
2021-12-31 18:51:17 +0700

    Remove unused code

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import path from 'path'
 import react from '@vitejs/plugin-react'
-import content from '@originjs/vite-plugin-content'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +10,5 @@ export default defineConfig({
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
-  plugins: [react(), content()],
+  plugins: [react()],
 })
